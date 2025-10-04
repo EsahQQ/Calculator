5d958d7247637fd28ad58370c65572bf47ca6540
Kovalev Ivan <kovalevvanc@gmail.com>
2025-10-04 21:58:59 +0300

    Add: not good sln without validation

--- Program.cs
+++ Program.cs
@@ -1,3 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Calculator;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Write("Enter a number: ");
+            var num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\t1. +");
+            Console.WriteLine("\t2. -");
+            Console.WriteLine("\t3. *");
+            Console.WriteLine("\t4. /");
+            Console.Write("Choose operation: ");
+            var operation = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            var num2 = Convert.ToInt32(Console.ReadLine());
+            int? result;
+            switch (operation)
+            {
+                case 1:
+                    result = num1 + num2;
+                    break;
+                case 2:
+                    result = num1 - num2;
+                    break;
+                case 3:
+                    result = num1 * num2;
+                    break;
+                case 4:
+                    result = num1 / num2;
+                    break;
+                default:
+                    result = null;
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
+            if (result == null)
+                continue;
+    
+            Console.WriteLine($"Result: {result}");
+            Console.WriteLine("One more? (1 - yes / 0 - no)");
+            var answer = Convert.ToInt32(Console.ReadLine());
+        }
 
-Console.WriteLine("Hello, World!");
+        return;
+    }
+}
+
